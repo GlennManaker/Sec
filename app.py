637dbd3b0ce3db09d2eb610fceea00ba50048650
vladimirfilipchenkov <vladimirfilipchenkov@yandex.by>
2020-11-06 22:59:33 +0300

    second commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import stripe
+from flask_cors import CORS
 stripe.api_key = "sk_test_51HkaJuH96yTZyVZDxrA8lRaKERxn6xdtYSLcWBBz22z1HxHGqqDFnhcsp61nFd8Kb0ok25yn1wDK9mULku7mWwJM008yNCeH9x"
 app = Flask(__name__)
+CORS(app)
 @app.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
   session = stripe.checkout.Session.create(

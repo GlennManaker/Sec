22d132b0e6b997c46feadc18d31edf930083d631
vladimirfilipchenkov <vladimirfilipchenkov@yandex.by>
2020-11-06 22:43:52 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import stripe
+stripe.api_key = "sk_test_51HkaJuH96yTZyVZDxrA8lRaKERxn6xdtYSLcWBBz22z1HxHGqqDFnhcsp61nFd8Kb0ok25yn1wDK9mULku7mWwJM008yNCeH9x"
+app = Flask(__name__)
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+  session = stripe.checkout.Session.create(
+    payment_method_types=['card'],
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'Deposit',
+        },
+        'unit_amount': request.headers['amount'],
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url= "https://yoursite.com/success.html",
+    cancel_url='https://example.com/cancel',
+  )
+
+  return jsonify(id=session.id)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/api/v1/balance')
+def get_balance():
+    return stripe.Balance.retrieve()
+
+if __name__ == '__main__':
+    app.run()

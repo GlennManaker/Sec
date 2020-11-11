cb476ebc420c829d62d0762f8bb8b9914e585fa5
vladimirfilipchenkov <vladimirfilipchenkov@yandex.by>
2020-11-12 00:28:34 +0300

    iii

--- routes/webhook/stripe_webhooks.py
+++ routes/webhook/stripe_webhooks.py
@@ -15,7 +15,7 @@ class Payment:
 @bps_webhook.route('/webhook', methods = ["POST"])
 def get_webhook():
         pay = json.loads(request.get_data())
-        print(pay['charges']['data'][0]['billing_details']['email'])
+        print(pay['data']['object']['charges']['data'][0]['billing_details']['email'])
         # if (pay['type'] == 'payment_intent.succeeded'):
         #     payment = Payment(pay['data'])
         #     app.db['payments'].insert_one(payment.toDict())
